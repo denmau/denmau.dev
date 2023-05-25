255e5e78d3ee82135afae8e9b38d5976dbd458e4
Dennis Kamau <103561493+denmau@users.noreply.github.com>
2023-05-25 13:04:20 +0300

    redesign landing page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('Contact: dennis[ at ]denmau[ dot ]me');
+
+const texts = ['Hello', '¡Hola!', 'Bonjour', 'Ciao', 'néih hóu', 'こんにちは'];
+const textElement = document.getElementById('hello-animation');
+
+let index = 0;
+let letterIndex = 0;
+
+function animate() {
+    const currentText = texts[index];
+    textElement.textContent = currentText.slice(0, ++letterIndex);
+
+    if (letterIndex === currentText.length) {
+        setTimeout(() => {
+            letterIndex = 0;
+            index = (index + 1) % texts.length;
+        }, 500);
+    }
+
+    setTimeout(animate, 400);
+}
+
+animate();
+console.log('Contact: dmkamau475-at-gmail-dot-com');
